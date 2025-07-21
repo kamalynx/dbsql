34db32ddd80a90b0913e9a5e68f2d0b5e3b2a723
kamalynx <kamalynxpro@gmail.com>
2025-07-21 09:05:09 +0500

    added pagination

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,7 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.Articles.as_view(), name='articles'),
+    path('page/<page>/', views.Articles.as_view(), name='pages'),
     path('<slug:category>/<slug:slug>/', views.Article.as_view(), name='article'),
     path('<slug:slug>/', views.Category.as_view(), name='category'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import last_modified
+from django.urls import reverse_lazy
+from django.conf import settings
 
 from articles import models
 
@@ -17,11 +19,24 @@ class Articles(ListView):
     queryset = models.Article.published.all()
     template_name = 'articles/index.html'
     context_object_name = 'articles'
+    paginate_by = getattr(settings, 'PAGINATE_BY', 2)
 
     def get_queryset(self):
         if not self.request.user.is_authenticated:
             return models.Article.published.all()
         return super().get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['canonical'] = self.request.build_absolute_uri(reverse_lazy('articles:articles'))
+
+        page_number = context['page_obj'].number
+        if page_number == 1:
+            context['title'] = self.request.site.name
+        else:
+            context['title'] = f'Страница {page_number} — {self.request.site.name}'
+
+        return context
 
 
 @method_decorator(last_modified(get_article_modified_date), name='dispatch')
